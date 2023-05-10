5e455c015c5ad402445805969c88ec3d6fc1c104
Rexva-00 <114014844+Rexva-00@users.noreply.github.com>
2023-05-10 12:32:52 -0700

    Add files via upload

--- brltty.sh
+++ brltty.sh
@@ -19,3 +19,13 @@ awk '/#speech-driver/ { print "\t" $2, $3, $4 }' $file
 # count number of speech drivers
 drivers_count=`grep "#speech-driver" $file | wc -l`
 echo "Number of speech drivers: $drivers_count"
+
+# The list of speech driver parameters (Missing driver:name=value,...) (It don't exist)
+echo " "
+echo "Types of speech parameters:"
+awk '/#speech-parameter/ {$1=""; print}' $file
+
+# The list of keyboard-tables (Missing off # no keyboard table) (It don't exist)
+echo " "
+echo "Types of keyboard tables:"
+awk '/#keyboard-table/ {$1=""; print}' $file
