f183d5736891d00e44578c10fa393ed6f1d29a7f
sudobeans <skywd7766@gmail.com>
2023-05-09 22:37:48 -0700

    add types of speech drivers

--- brltty.sh
+++ brltty.sh
@@ -11,6 +11,11 @@ fi
 # name of file
 file=$1
 
+# get list of speech drivers
+# (our code does not print ViaVoice; Voxin. is this expected?)
+echo "Types of speech drivers:"
+awk '/#speech-driver/ { print "\t" $2, $3, $4 }' $file
+
 # count number of speech drivers
 drivers_count=`grep "#speech-driver" $file | wc -l`
 echo "Number of speech drivers: $drivers_count"
